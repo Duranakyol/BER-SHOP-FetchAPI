93dc9f01d9a1bd6d4815c4d41deaf81f849c7730
abc <duranakyol71@gmail.com>
2023-04-13 17:56:09 +0200

    add Login-Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+// import { delCart } from "../redux/action";
 import "../assets/styles/Cart.css";
 
-function Cart() {
-  const state = useSelector((state) => state.addItem);
+const Cart = () => {
+  const state = useSelector((state) => state.addCart);
   const dispatch = useDispatch();
 
   const cartItems = (cartItem) => {
     return (
-      <div className="px-4 my-5 bg-light rounded-3">
-        <div className="container py-4">
-          <button className="btn-close float-end" aria-label="Close"></button>
-          <div className="row justify-content-center">
-            <div className="col-md-4">
-              <img src={cartItem.img} alt="cartItem" height="200px" />
+      <div className="cartContainer" key={cartItem.id}>
+        <div className="box">
+          <button className="btn">MyButton</button>
+          <div>
+            <div>
+              <img src={cartItem.img} alt={cartItem.title} />
             </div>
-            <div className="col-md-4">
-              <h3>h3</h3>
-              <p></p>
+            <div>
+              <h3>{}</h3>
+              <p> {} </p>
             </div>
           </div>
         </div>
       </div>
     );
   };
-}
+
+  // const dellProduct = (product) => {
+  //   dispatch(delCart(product));
+  // };
+
+  return <>{state.length !== 0 && state.map(cartItems)}</>;
+};
 
 export default Cart;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,103 @@
 import React from "react";
 import "../assets/styles/Login.css";
+import Logo from "../assets/img/shop.png";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  return <div className="login">Wir arbeiten darüber</div>;
+  return (
+    <div className="Auth">
+      <div className="a-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <span>BERSHOP</span>
+          <h5>Sichere und Schnell Einkaufen</h5>
+        </div>
+      </div>
+      <LogInForm />
+    </div>
+  );
 };
+
+function LogInForm() {
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
+
+  let navigate = useNavigate();
+  const routeChange = () => {};
+
+  return (
+    <div className="login">
+      <h2>LOGIN</h2>
+
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={(values) => {
+          toast.success("Erfolgreicher Eingeloggt!", {
+            icon: "🚀",
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          setTimeout(() => {
+            navigate("/home");
+          }, 3000);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="loginForm">
+            <Field name="email" type="email" placeholder="Enter your Email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <Field
+              name="password"
+              type="password"
+              placeholder="Enter your Password"
+            />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+
+            <span className="mySpan">
+              Don't you have a Account?<a href="/signup"> Sign Up</a>
+            </span>
+
+            <button className="loginButton" type="submit" onClick={routeChange}>
+              Login
+            </button>
+            <ToastContainer
+              position="top-center"
+              autoClose={2000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
 
 export default Login;
